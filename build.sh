15a9c89c04e358cb503d10145a92c7c6897568fa
jesperkha <hammerjesp@gmail.com>
2022-02-27 01:18:38 +0100

    SDL2 setup

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 set -e
-[ ! -d "./bin" ] && mkdir bin
+[ ! -d "bin" ] && mkdir bin
 
-FLAGS="-Wall -Werror -Wextra -O2 -std=c99"
-INCLUDES="main.c emulator/*.c"
-gcc $FLAGS -o bin/out.exe $INCLUDES
+FLAGS="-Wall -Werror -std=c99"
+FILES="../main.c ../emulator/*.c"
+
+pushd bin
+gcc -c $FILES $FLAGS -I ../include -I ~/Coding/C/SDL2/include
+gcc *.o -o out.exe -L ~/Coding/C/SDL2/lib -lmingw32 -lSDL2main -lSDL2
+popd
+
+# rm bin/*.o
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main()
+int main(int argc, char** argv)
 {
 	printf("Testing...\n");
 	return 0;
